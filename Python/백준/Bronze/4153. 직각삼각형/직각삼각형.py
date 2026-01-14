54753609929f01af648f0dd007098617401a4dba
edodow <fritzlove8@gmail.com>
2026-01-14 20:50:47 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ Python/백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,23 +1,14 @@
-while 1:
-    line = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
+
+while True:
+    line = sorted(map(int, input().split()))
     if (line[0] == 0):
         break
 
-    # 주어진 변의 길이가 삼각형이 될 수 없다면? 30000보다 작은 양수
-    line.sort()
-    if (line[0] + line[1] < line[2]):
-        print("wrong")
-        continue
-
     [a, b, c] = line
 
-
-    # 세개 중 어느게 빗변이 되는지 모르는 상태가 아닌가?
     if (a*a + b*b == c*c):
-        print("right")
-    elif (b*b + c*c == a*a):
-        print("right")
-    elif (c*c + a*a == b*b):
         print("right")
     else:
         print("wrong")
