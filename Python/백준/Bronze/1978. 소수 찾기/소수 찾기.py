0f8c52c8857214c4a01eea8932c778ce4b8c73a4
edodow <fritzlove8@gmail.com>
2026-01-16 02:44:03 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+is_prime = [False, False] + [True] * 999 
+
+for i in range(2, 32): 
+    if is_prime[i]:
+        for j in range(i*i, 1001, i):
+            is_prime[j] = False
+
+cnt = 0
+for n in nums:
+    if is_prime[n]:
+        cnt += 1
+
+print(cnt)
