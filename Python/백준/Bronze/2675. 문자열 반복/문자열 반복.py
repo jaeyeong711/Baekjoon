06a95f472ef1490242f5a9f913f3b5a3db442b81
edodow <fritzlove8@gmail.com>
2026-01-16 23:47:17 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    R, S = input().split()
+    R = int(R)
+    for j in S:
+        print(j * R, end = '')
+    print('')
