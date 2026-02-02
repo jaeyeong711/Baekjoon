bcf8c89512d4ab23d85b81b08f13669bf3a19640
jaeyeong711 <fritzlove8@gmail.com>
2026-02-02 22:41:53 +0900

    [Bronze I] Title: ISBN, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/14626. ISBN/ISBN.py
+++ Python/백준/Bronze/14626. ISBN/ISBN.py
@@ -4,18 +4,15 @@ def solve(isbn):
     isbn = isbn.replace('*', '0')
     isbn = [int(isbn[i]) if i % 2 == 0 else int(isbn[i]) * 3 for i in range(len(isbn))]
 
+    if sum(isbn) % 10 == 0:
+        return 0
+
     if index % 2 == 0:
-        if sum(isbn) % 10 == 0:
-            return 0
-        else:
-            return 10 - (sum(isbn) % 10)
+        return 10 - (sum(isbn) % 10)
     else:
-        if sum(isbn) % 10 == 0:
-            return 0
-        else:
-            for i in range(1, 10):
-                if (i * 3 + sum(isbn)) % 10 == 0:
-                    return i
+        for i in range(1, 10):
+            if (i * 3 + sum(isbn)) % 10 == 0:
+                return i
 
 
 ISBN = input()
