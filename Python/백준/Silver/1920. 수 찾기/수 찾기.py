cf040774285cc91d0d19d73d519fcef5ae0e3e5d
jaeyeong711 <fritzlove8@gmail.com>
2026-02-03 22:47:54 +0900

    [Silver IV] Title: 수 찾기, Time: 104 ms, Memory: 54000 KB -BaekjoonHub

--- Python/백준/Silver/1920. 수 찾기/수 찾기.py
+++ Python/백준/Silver/1920. 수 찾기/수 찾기.py
@@ -3,12 +3,7 @@ input = sys.stdin.readline
 
 def search(nums, find):
     exists = set(nums) & set(find)
-    unexists = set(find) - set(nums)
-    
-    if len(exists) >= len(unexists):
-        _list = ['0' if n in unexists else '1' for n in find]
-    else:
-        _list = ['1' if n in exists else '0' for n in find]
+    _list = ['1' if n in exists else '0' for n in find]
     print('\n'.join(_list))
 
 
